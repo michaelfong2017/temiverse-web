e11d832c9abfc4fc3ec097c5991101f52e6dc6c4
michaelfong2017 <michaelfong2017@gmail.com>
2022-08-18 13:17:42 +0800

    Add robot control

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,45 +2,51 @@ import { useEffect, useRef, useState } from "react"
 import Layout from "../components/layout"
 
 const useWidth = () => {
-  const [width, setWidth] = useState(0); // default width
-  const handleResize = () => setWidth(window.innerWidth);
+  const [width, setWidth] = useState(0) // default width
+  const handleResize = () => setWidth(window.innerWidth)
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [handleResize]);
-  return width;
-};
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [handleResize])
+  return width
+}
 
 const useHeight = () => {
-  const [height, setHeight] = useState(0); // default height
-  const handleResize = () => setHeight(window.innerHeight);
+  const [height, setHeight] = useState(0) // default height
+  const handleResize = () => setHeight(window.innerHeight)
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [handleResize]);
-  return height;
-};
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [handleResize])
+  return height
+}
 
 export default function IndexPage() {
   const vr = useRef<HTMLIFrameElement>(null)
 
-  const windowWidth = useWidth();
-  const windowHeight = useHeight();
+  const windowWidth = useWidth()
+  const windowHeight = useHeight()
 
-  useEffect(() => {
-    vr.current!.src += ""
-  }, [vr, windowWidth, windowHeight])
+  // useEffect(() => {
+  //   vr.current!.src += ""
+  // }, [vr, windowWidth, windowHeight])
 
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
       <iframe
+        src="https://stream.robocore.ai:5443/LiveApp/play.html?name=stream1&is360=true"
+        width="960"
+        height="540"
+      />
+      <iframe src="http://18.166.70.71:8000/" width="480" height="270" />
+      {/* <iframe
         ref={vr}
         src="http://18.163.8.140:8081/index.html"
         width="80%"
         height="50%"
       />
-      <iframe src="http://18.163.8.140:5000/" width="50%" height="30%" />
+      <iframe src="http://18.163.8.140:5000/" width="50%" height="30%" /> */}
       <p>
         This is an example site to demonstrate how to use{" "}
         <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.

83182e29cc9f2cdd28e12670ed6348ab10455df3
michaelfong2017 <michaelfong2017@gmail.com>
2022-09-14 18:01:04 +0800

    Select booth ui only.

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -1,26 +1,36 @@
-import React, { useReducer } from "react";
-import Head from "next/head";
-import DropZone from "../../components/DropZone";
-import styles from "../../styles/Upload.module.css";
+import React, { useReducer } from "react"
+import Head from "next/head"
+import DropZone from "../../components/DropZone"
+import styles from "../../styles/Upload.module.css"
+import { Dropdown } from "@nextui-org/react"
 
 export default function Upload() {
   // reducer function to handle state changes
   const reducer = (state, action) => {
     switch (action.type) {
       case "SET_IN_DROP_ZONE":
-        return { ...state, inDropZone: action.inDropZone };
+        return { ...state, inDropZone: action.inDropZone }
       case "ADD_FILE_TO_LIST":
-        return { ...state, fileList: state.fileList.concat(action.files) };
+        return { ...state, fileList: state.fileList.concat(action.files) }
       default:
-        return state;
+        return state
     }
-  };
+  }
 
   // destructuring state and dispatch, initializing fileList to empty array
   const [data, dispatch] = useReducer(reducer, {
     inDropZone: false,
     fileList: [],
-  });
+  })
+
+  /** Booth dropdown */
+  const [selected, setSelected] = React.useState(new Set(["1"]))
+
+  const selectedValue = React.useMemo(
+    () => Array.from(selected).join(", ").replaceAll("_", " "),
+    [selected]
+  )
+  /** Booth dropdown END */
 
   return (
     <div className={styles.container}>
@@ -32,6 +42,29 @@ export default function Upload() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Drag And Drop File Upload</h1>
+
+        <h3>Select booth:</h3>
+        <Dropdown>
+          <Dropdown.Button flat color="secondary" css={{ tt: "capitalize" }}>
+            {selectedValue}
+          </Dropdown.Button>
+          <Dropdown.Menu
+            aria-label="Single selection actions"
+            color="secondary"
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selected}
+            onSelectionChange={setSelected}
+          >
+            <Dropdown.Item key="1">1</Dropdown.Item>
+            <Dropdown.Item key="2">2</Dropdown.Item>
+            <Dropdown.Item key="3">3</Dropdown.Item>
+            <Dropdown.Item key="4">4</Dropdown.Item>
+            <Dropdown.Item key="5">5</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <br />
         {/* Pass state data and dispatch to the DropZone component */}
         <DropZone data={data} dispatch={dispatch} />
       </main>
@@ -40,5 +73,5 @@ export default function Upload() {
         <div>{new Date().getFullYear()}</div>
       </footer>
     </div>
-  );
+  )
 }
